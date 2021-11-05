944c25bcf2c926ea762637c43dfdd7fa8742d085
Naomie <naomie.discala@gmail.com>
2021-11-05 14:52:20 +0100

    Test

--- panier.php
+++ panier.php
@@ -10,8 +10,15 @@ if( isset($_POST['ajout_panier'] ) ){ //Ici, on vérifie l'existence d'un "submi
     //Ici, $_POST[id_produit] provient de l'input type hidden dans le formulaire du fichierfiche_produit.php
 
     $produit = $r->fetch(PDO::FETCH_ASSOC);
-    debug($produit);
+    // debug($produit);
 
+    //appel de la fonction creation_panier
+    creation_panier();
+    ajout_panier($produit['titre'], $_POST['id_produit'], $_POST['quantite'], $produit['prix']);
+    //Ici, la quantité et lidproduit proviennet du formulaire de fiche produit.php (donc du post)
+    //le titre et le prix proviennent de la requete ci dessus
+
+    debug($_SESSION);
 }
 ?>
 
